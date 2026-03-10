e4a5b0eee01c46962bd189cc8f9114b2ad1c8442
rahulltanvir <tanvirrahul00000@gmail.com>
2026-03-11 00:42:55 +0600

    inheritance

--- php_inheritance.php
+++ php_inheritance.php
@@ -1,1 +1,20 @@
-<?php ?>
+<?php 
+    Class stdName{
+        public $name;
+
+        public function fullName($n){
+            $this->name=$n;
+        }
+    } 
+
+    Class stdRoll extends stdName{
+        public function stdRoll() {
+            echo "Roll No:30 ".$this->name;
+        }
+
+    }
+
+    $stdInfo = new stdRoll();
+    $stdInfo->fullName("Rahul");
+    $stdInfo->stdRoll();
+?>
